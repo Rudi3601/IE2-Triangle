bf8014a0729594cc5bc600e5a6f15d4edb9ab876
Rudi3601 <e771d87a-7c54-479b-b3d0-771630b67da9@4146c987a9a6>
2023-10-26 17:42:54 +0000

    test_statementCoverage.py test suite added

---
+++ test_suit/test_statementCoverage.py
@@ -0,0 +1,43 @@
+import unittest
+from isTriangle import Triangle
+
+class StatementTest(unittest.TestCase):
+    def test_equilateral_triangle(self):
+        actual = Triangle.classify(10, 10, 10)
+        expected = Triangle.Type.EQUILATERAL
+        self.assertEqual(actual, expected)
+        
+    def test_isosceles_triangle1(self):
+        actual = Triangle.classify(10, 10, 13)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+        
+    def test_isosceles_triangle2(self):
+        actual = Triangle.classify(13, 10, 10)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+        
+    def test_isosceles_triangle3(self):
+        actual = Triangle.classify(10, 13, 10)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+        
+    def test_scalene_triangle(self):
+        actual = Triangle.classify(3, 4, 5)
+        expected = Triangle.Type.SCALENE
+        self.assertEqual(actual, expected)
+        
+    def test_invalid_triangle1(self):
+        actual = Triangle.classify(-3, 4, 5)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+    def test_invalid_triangle2(self):
+        actual = Triangle.classify(5, 8, 15)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
